7fe41c6e9e644164ffd7b61727f4c63bc7211460
mraheeldev <m.raheeldev@gmail.com>
2022-04-29 12:21:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/motionlayoutsplashscreen/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.motionlayoutsplashscreen
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.motionlayoutsplashscreen.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@MainActivity, SecondActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
+    }
+}
---
+++ app/src/main/java/com/example/motionlayoutsplashscreen/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.example.motionlayoutsplashscreen
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
